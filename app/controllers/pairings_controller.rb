232e8b907edf838d7ab151c5645161d582b2c0a1
Bruno <bruno.soares.guimaraes@gmail.com>
2023-06-10 10:37:18 -0300

    Função de cadastrar duplas manualmente no histórico

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -11,7 +11,17 @@ class PairingsController < ApplicationController
     redirect_to root_path, alert: e.message
   end
 
+  def add_manual_pair
+    pair1 = params[:participant1] + " e " + params[:participant2]
+    pair2 = params[:participant2] + " e " + params[:participant1]
 
+    if Pairing.exists?(pair: pair1) || Pairing.exists?(pair: pair2)
+      redirect_to pairings_path, alert: 'Esta dupla já existe no histórico'
+    else
+      Pairing.create(pair: pair1)
+      redirect_to pairings_path, notice: 'Dupla adicionada com sucesso'
+    end
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-root 'pairings#index'
+  root 'pairings#index'
 
-resources :pairings, only: [:index, :create]
+  resources :pairings, only: [:index, :create]
 
-resources :participants, only: [:create]
+  resources :participants, only: [:create]
 
+  post 'pairings/manual', to: 'pairings#add_manual_pair', as: 'manual_pairs'
 end
+
